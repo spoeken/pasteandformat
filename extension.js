ea3cab64bf2582105d159e40d075ae5b61f408e6
Mathias Stav <mathias@feed.no>
2016-01-22 14:34:33 +0100

    Conditional to open markdown preview if language mode is set to markdown.

--- extension.js
+++ extension.js
@@ -1,53 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var window = vscode.window;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "pasteandformat" is now active!'); 
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     var disposable = vscode.commands.registerCommand('extension.pasteAndFormat', function () {
-        // console.log(window.activeTextEditor.document);
-        // console.log(window.activeTextEditor.selection);
         
-        // Get current cursor position
-        var start = window.activeTextEditor.selection.anchor;
-        //Paste from clipboard
-        vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(function () {
+        
+        //Check if current document is markdown
+        
+        if(window.activeTextEditor.document.languageId === 'markdown'){
+            vscode.commands.executeCommand('workbench.action.markdown.togglePreview');
+        } else {
+            // Save current position
+            var start = window.activeTextEditor.selection.anchor;
+            // Paste from clipboard
+            vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(function () {
 
-            var end = window.activeTextEditor.selection.anchor; // Get cursor position after paste
-            var selection = new vscode.Selection(start.line, start.character, end.line, end.character); // Create selection
-            window.activeTextEditor.selection = selection; //Apply selection to editor
-            
+                var end = window.activeTextEditor.selection.anchor; // Get position after paste
+                var selection = new vscode.Selection(start.line, start.character, end.line, end.character); // Create selection
+                window.activeTextEditor.selection = selection; // Apply selection to editor
                 
-            // Format selection, when text is selected, that text is the only thing that will be formatted
-            vscode.commands.executeCommand('editor.action.format').then(function () {
-                // This is where I really would like the deselection to happen
-                // but it runs before formatting is done, I've tried window.onDidChangeTextEditorSelection, but that doesn't seem to work properly eighther
-                // Until issue #1775 is solved I just use a timeout. 
+                // Format selection, when text is selected, that text is the only thing that will be formatted
+                vscode.commands.executeCommand('editor.action.format').then(function () {
+                    // This is where I really would like the deselection to happen but it runs before
+                    // formatting is done, I've tried window.onDidChangeTextEditorSelection, but that doesn't
+                    // seem to work how I would like it to eighther.
+                    // Until issue #1775 is solved I just use a timeout. 
 
-                setTimeout(function () {
-                    // Hopefully the format command is done when this happens
-                    var line = window.activeTextEditor.selection.end.line;
-                    var character = window.activeTextEditor.selection.end.character;
-                    //Set both start and end of selection to the same point so that nothing is selected
-                    var newSelection = new vscode.Selection(line, character, line, character); // Create selection
-                    window.activeTextEditor.selection = newSelection;
-                }, 100);
+                    setTimeout(function () {
+                        // Hopefully the format command is done when this happens
+                        var line = window.activeTextEditor.selection.end.line;
+                        var character = window.activeTextEditor.selection.end.character;
+                        // Set both start and end of selection to the same point so that nothing is selected
+                        var newSelection = new vscode.Selection(line, character, line, character); // Create selection
+                        window.activeTextEditor.selection = newSelection; // Apply selection to editor
+                    }, 100);
 
+                });
 
-            });
-
-
-        });
+            });            
+        }
+        
+       
 
     });
 
