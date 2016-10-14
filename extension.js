15c95d259059c0a8f20831c94838fdc9391afff2
JakeDOD <jakedalyodonnell@gmail.com>
2016-10-14 12:35:16 -0400

    Fix formating on paste over selected text.
    TextEditor.selection.active represents the position of the cursor. This fixes an issue when pasting over text selected from the bottom up.

--- extension.js
+++ extension.js
@@ -4,28 +4,28 @@ var window = vscode.window;
 function activate(context) {
 
     var disposable = vscode.commands.registerCommand('extension.pasteAndFormat', function () {
-        
-        
+
+
         //Check if current document is markdown
-        
+
         if(window.activeTextEditor.document.languageId === 'markdown'){
             vscode.commands.executeCommand('workbench.action.markdown.togglePreview');
         } else {
             // Save current position
-            var start = window.activeTextEditor.selection.anchor;
+            var start = window.activeTextEditor.selection.active;
             // Paste from clipboard
             vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(function () {
 
-                var end = window.activeTextEditor.selection.anchor; // Get position after paste
+                var end = window.activeTextEditor.selection.active; // Get position after paste
                 var selection = new vscode.Selection(start.line, start.character, end.line, end.character); // Create selection
                 window.activeTextEditor.selection = selection; // Apply selection to editor
-                
+
                 // Format selection, when text is selected, that text is the only thing that will be formatted
                 vscode.commands.executeCommand('editor.action.format').then(function () {
                     // This is where I really would like the deselection to happen but it runs before
                     // formatting is done, I've tried window.onDidChangeTextEditorSelection, but that doesn't
                     // seem to work how I would like it to eighther.
-                    // Until issue #1775 is solved I just use a timeout. 
+                    // Until issue #1775 is solved I just use a timeout.
 
                     setTimeout(function () {
                         // Hopefully the format command is done when this happens
@@ -38,10 +38,10 @@ function activate(context) {
 
                 });
 
-            });            
+            });
         }
-        
-       
+
+
 
     });
 
